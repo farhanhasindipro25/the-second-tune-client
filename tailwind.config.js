c80c5dc7dc68d320c7a04894d895ec577393ed37
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-11-24 23:26:54 +0600

    Connected to private repository, changed favicon

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        theSecondTuneTheme: {
+          primary: "#12263a", // dark blue
+          accent: "#F2F5FF", // white
+          error: "#EF3E36", // red
+          success: "#00C49A", // green
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
