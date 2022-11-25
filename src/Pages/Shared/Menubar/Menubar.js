e308e65dbd9bafe24cdd916aa1f10871dd3bc1c7
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-11-26 04:52:25 +0600

    Conditional Rendering on navbar added

--- src/Pages/Shared/Menubar/Menubar.js
+++ src/Pages/Shared/Menubar/Menubar.js
@@ -71,12 +71,54 @@ const Menubar = () => {
         </div>
         <div className="navbar-end">
           {user?.uid ? (
-            <button
-              className="btn btn-success btn-outline"
-              onClick={handleLogOut}
-            >
-              <Link>LOG OUT</Link>
-            </button>
+            <div>
+              {user?.photoURL ? (
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="w-8 h-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                      <img src={user.photoURL} alt="" />
+                    </div>
+                  </div>
+                  <div>
+                    <h2>Welcome, {user.displayName}</h2>
+                  </div>
+                  <button
+                    className="btn btn-success btn-outline"
+                    onClick={handleLogOut}
+                  >
+                    <Link>LOG OUT</Link>
+                  </button>
+                </div>
+              ) : (
+                <div className="flex items-center gap-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-8 h-8"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  {user?.displayName && (
+                    <div>
+                      <h2>Welcome {user.displayName}</h2>
+                    </div>
+                  )}
+                  <button
+                    className="btn btn-success btn-outline"
+                    onClick={handleLogOut}
+                  >
+                    <Link>LOG OUT</Link>
+                  </button>
+                </div>
+              )}
+            </div>
           ) : (
             <button className="btn btn-success btn-outline">
               <Link to="/login">LOGIN</Link>
