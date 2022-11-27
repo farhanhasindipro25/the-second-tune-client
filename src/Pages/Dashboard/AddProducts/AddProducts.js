99d4b76264aef108b5863bc6d29511c44d50ce62
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-11-27 17:00:23 +0600

    Add Products form updated

--- src/Pages/Dashboard/AddProducts/AddProducts.js
+++ src/Pages/Dashboard/AddProducts/AddProducts.js
@@ -9,87 +9,122 @@ const AddProducts = () => {
         ADD PRODUCTS
       </h2>
       <form className="mt-10 mb-10 bg-secondary p-5 rounded-xl">
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="ProductName">Product Name</label>
-          <input
-            type="text"
-            name="productName"
-            placeholder="Enter Product Name"
-            className="input input-bordered input-success w-full text-secondary"
-          />
+        <div className="flex gap-2 my-6">
+          <div className="w-3/4">
+            <label className="font-semibold" htmlFor="ProductName">
+              Product Name
+            </label>
+            <input
+              type="text"
+              name="productName"
+              placeholder="Enter Product Name"
+              className="input input-bordered input-success w-full text-secondary"
+            />
+          </div>
+          <div className="w-1/4">
+            <label className="font-semibold" htmlFor="ProductName">
+              Product Photo
+            </label>
+            <input
+              type="file"
+              className="file-input file-input-bordered file-input-success text-secondary font-medium w-full"
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="ProductCategory">Product Category</label>
-          <select className="select select-success text-secondary w-full">
-            <option className="text-secondary">Guitars</option>
-            <option className="text-secondary">Amplifiers</option>
-            <option className="text-secondary">Processors</option>
-            <option className="text-secondary">Pedals</option>
-            <option className="text-secondary">Keyboards</option>
-            <option className="text-secondary">Drums</option>
-          </select>
+        <div className="flex gap-2 my-6">
+          <div className="w-1/2">
+            <label className="font-semibold" htmlFor="ProductCategory">
+              Product Category
+            </label>
+            <select className="select select-success text-secondary w-full">
+              <option className="text-secondary">Guitars</option>
+              <option className="text-secondary">Amplifiers</option>
+              <option className="text-secondary">Processors</option>
+              <option className="text-secondary">Pedals</option>
+              <option className="text-secondary">Keyboards</option>
+              <option className="text-secondary">Drums</option>
+            </select>
+          </div>
+          <div className="w-1/2">
+            <label className="font-semibold" htmlFor="ProductCondition">
+              Product Condition
+            </label>
+            <select className="select select-success text-secondary w-full">
+              <option className="text-secondary">Excellent</option>
+              <option className="text-secondary">Good</option>
+              <option className="text-secondary">Fair</option>
+            </select>
+          </div>
         </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="ProductCondition">Product Condition</label>
-          <select className="select select-success text-secondary w-full">
-            <option className="text-secondary">Excellent</option>
-            <option className="text-secondary">Good</option>
-            <option className="text-secondary">Fair</option>
-          </select>
+        <div className="flex gap-2 my-6">
+          <div className="w-1/2">
+            <label className="font-semibold" htmlFor="SellingPrice">
+              Selling Price
+            </label>
+            <input
+              type="number"
+              name="sellingPrice"
+              placeholder="Enter Selling Price"
+              className="input input-bordered input-success w-full text-secondary"
+            />
+          </div>
+          <div className="w-1/2">
+            <label className="font-semibold" htmlFor="BuyingPrice">
+              Buying Price
+            </label>
+            <input
+              type="number"
+              name="buyingPrice"
+              placeholder="Enter Buying Price"
+              className="input input-bordered input-success w-full text-secondary"
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="SellingPrice">Selling Price</label>
-          <input
-            type="number"
-            name="sellingPrice"
-            placeholder="Enter Selling Price"
-            className="input input-bordered input-success w-full text-secondary"
-          />
+        <div className="flex gap-2 my-6">
+          <div className="w-1/3">
+            <label className="font-semibold" htmlFor="timeUsed">
+              Time Used
+            </label>
+            <input
+              type="text"
+              name="timeUsed"
+              placeholder="Enter Time Used (days/weeks/months/years)"
+              className="input input-bordered input-success w-full text-secondary"
+            />
+          </div>
+          <div className="w-1/3">
+            <label className="font-semibold" htmlFor="ProductName">
+              Phone Number
+            </label>
+            <input
+              type="number"
+              name="phoneNumber"
+              placeholder="Enter Phone Number"
+              className="input input-bordered input-success w-full text-secondary"
+            />
+          </div>
+          <div className="w-1/3">
+            <label className="font-semibold" htmlFor="ProductName">
+              Location
+            </label>
+            <input
+              type="text"
+              name="location"
+              placeholder="Enter Location"
+              className="input input-bordered input-success w-full text-secondary"
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="BuyingPrice">Buying Price</label>
-          <input
-            type="number"
-            name="buyingPrice"
-            placeholder="Enter Buying Price"
-            className="input input-bordered input-success w-full text-secondary"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="timeUsed">Time Used</label>
-          <input
-            type="text"
-            name="timeUsed"
-            placeholder="Enter Time Used (days/weeks/months/years)"
-            className="input input-bordered input-success w-full text-secondary"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="ProductName">Phone Number</label>
-          <input
-            type="number"
-            name="phoneNumber"
-            placeholder="Enter Phone Number"
-            className="input input-bordered input-success w-full text-secondary"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="ProductName">Location</label>
-          <input
-            type="text"
-            name="location"
-            placeholder="Enter Location"
-            className="input input-bordered input-success w-full text-secondary"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label htmlFor="ProductDescription">Product Description</label>
+        <div className="flex flex-col">
+          <label className="font-semibold" htmlFor="ProductDescription">
+            Product Description
+          </label>
           <textarea
             className="textarea textarea-success text-secondary"
             placeholder="Product Description"
           ></textarea>
         </div>
-        <button className="btn btn-success w-full">ADD PRODUCT</button>
+        <button className="btn btn-success w-full my-6">ADD PRODUCT</button>
       </form>
     </div>
   );
